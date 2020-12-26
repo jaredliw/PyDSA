0034e942ecdf3b6b08e58812ccaf3fb0b45278d4
jaredliw <jaredliw@gmail.com>
2020-12-26 16:49:07 +0800

    New DS: DynamicArray, turn StaticArray._max_length into private, redefine __eq__, etc.

--- pydsa/data_structures/array.py
+++ pydsa/data_structures/array.py
@@ -29,16 +29,40 @@ class StaticArray(list):
         if self.max_length < len(self):
             raise ExceedMaxLengthError("Exceed static array maximum length: {}".format(self.max_length))
 
+    def __add__(self, other):
+        return self.__class__(super(StaticArray, self).__add__(other))
+        
     def __delattr__(self, item):
         raise AttributeError("Attribute '{}' could not be deleted".format(item))
 
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and super(StaticArray, self).__eq__(other)
+
+    def __ge__(self, other):
+        return self.__gt__(other) or self.__eq__(other)
+    
     def __getattribute__(self, item):
-        if item in ['append', 'insert'] and super(StaticArray, self).__len__() + 1 > self.max_length:
+        if item in ["append", "insert"] and super(StaticArray, self).__len__() + 1 > self.max_length:
             raise ExceedMaxLengthError("Exceed static array maximum length: {}".format(self.max_length))
         return super(StaticArray, self).__getattribute__(item)
 
+    def __gt__(self, other):
+        return not self.__lt__(other)
+
+    def __le__(self, other):
+        return self.__eq__(other) or self.__lt__(other)
+
+    def __lt__(self, other):
+        return isinstance(other, self.__class__) and super(StaticArray, self).__le__(other)
+
+    def __mul__(self, other):
+        return self.__class__(super(StaticArray, self).__mul__(other))
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
     def __repr__(self):
-        return "StaticArray({}, {})".format(super(StaticArray, self).__repr__(), self.max_length)
+        return "{}({}, {})".format(self.__class__.__name__, super(StaticArray, self).__repr__(), self.max_length)
 
     def __str__(self):
         return super(StaticArray, self).__repr__()
@@ -46,23 +70,89 @@ class StaticArray(list):
     @property
     def max_length(self):
         """A constant maximum length of StaticArray."""
-        return self._max_length
+        return self.__max_length
 
     @max_length.setter
     def max_length(self, value):
-        if not hasattr(self, "_max_length"):
-            self._max_length = value
+        if not hasattr(self, "_{}__max_length".format(self.__class__.__name__)):
+            self.__max_length = value
         else:
             raise ConstantError("StaticArray.max_length is a constant")
 
     @validate_args
     def copy(self):
         """Override list copy() function so that it returns a StaticArray object."""
-        return StaticArray(super(StaticArray, self).copy(), self.max_length)
+        if self.__class__ == StaticArray:
+            return self.__class__(super(StaticArray, self).copy(), self.max_length)
+        else:
+            return self.__class__(super(StaticArray, self).copy())
 
     @validate_args
     def extend(self, iterable: Iterable) -> None:
         """Override list extend() function to ensure the length of the list is always below self.max_length."""
-        if len(self) + len(iterable) > self.max_length:
+        if len(self) + len(list(iterable)) > self.max_length:
             raise ExceedMaxLengthError('Exceed StaticArray maximum length: {}'.format(self.max_length))
         super(StaticArray, self).extend(iterable)
+
+
+class DynamicArray(StaticArray):
+    """Array (implemented on StaticArray) with dynamic length."""
+
+    # Conceptual, need not to use in Python.
+
+    @validate_args
+    def __init__(self, arr: Iterable = None):
+        if arr is None:
+            arr = []
+        super(DynamicArray, self).__init__(arr)
+
+    def __getattribute__(self, item):
+        if item in ['append', 'insert'] and self.__len__() + 1 > self.max_length:
+            self.__expend_array()
+        return super(DynamicArray, self).__getattribute__(item)
+
+    @validate_args
+    def __expend_array(self) -> None:
+        size = self.max_length
+        if size == 0:
+            size = 1
+        else:
+            size *= 2
+        super(DynamicArray, self).__init__(self, size)
+
+    @validate_args
+    def __shrink_array(self, old_length: int) -> None:
+        size = old_length // 2
+        super(DynamicArray, self).__init__(self, size)
+
+    @validate_args
+    def clear(self) -> None:
+        """Override StaticArray clear() function so that the qarray will shrink."""
+        super(DynamicArray, self).__init__([])
+
+    @validate_args
+    def pop(self, index: int = -1) -> None:
+        """Override StaticArray pop() function so that the list shrink when element is removed."""
+        old_length = self.max_length
+        super(DynamicArray, self).pop(index)
+        if self.__len__() <= old_length // 2:
+            self.__shrink_array(old_length)
+
+    @validate_args
+    def extend(self, iterable: Iterable) -> None:
+        """Override StaticArray extend() function to expand the list when exceeding the max_length of StaticArray."""
+        if not hasattr(iterable, "__len__"):
+            length = len(list(iterable))
+        else:
+            length = len(iterable)
+        while self.__len__() + length > self.max_length:
+            self.__expend_array()
+        super(DynamicArray, self).extend(iterable)
+
+    @validate_args
+    def remove(self, elem: Any) -> None:
+        """Override list remove() function so that the list shrink when element is removed."""
+        old_length = self.max_length
+        super(DynamicArray, self).remove(elem)
+        if self.__len__() <= old_length // 2:
+            self.__shrink_array(old_length)
--- tests/test_array.py
+++ tests/test_array.py
@@ -1,103 +1,129 @@
-from pydsa.data_structures.array import ConstantError, ExceedMaxLengthError, StaticArray
+from pydsa.data_structures.array import ConstantError, DynamicArray, ExceedMaxLengthError, StaticArray
 from tests import is_error
+
+ds = [StaticArray, DynamicArray]
 
 
 def test_init():
-    StaticArray([1, 2, 3], 10)
+    for array in ds:
+        array([])
+        array([1, 2, 3])
+        is_error(TypeError, array, 123)
+        is_error(TypeError, array, [], 1.2)
+
+    StaticArray([7, 8, 9], 10)
     is_error(ExceedMaxLengthError, StaticArray, [1, 2, 3], 1)
-    is_error(TypeError, StaticArray, 123)
-    is_error(TypeError, StaticArray, [], 1.2)
 
 
 def test_op():
-    assert StaticArray([1, 2, 3], 6) + StaticArray([4, 5, 6], 5) == StaticArray([1, 2, 3, 4, 5, 6], 7)
-    a = StaticArray([7, 8, 9])
-    a += StaticArray([10, 11, "Test"])
-    assert a == StaticArray([7, 8, 9, 10, 11, "Test"])
+    for array in ds:
+        assert array([1, 2, 3]) + array([4, 5, 6]) == array([1, 2, 3, 4, 5, 6])
+        a = array([7, 8, 9])
+        a += array([10, 11, "Test"])
+        assert a == array([7, 8, 9, 10, 11, "Test"])
 
-    assert StaticArray([1, 3, 5]) * 3 == StaticArray([1, 3, 5, 1, 3, 5, 1, 3, 5])
-    a = StaticArray([2, 4, "a"])
-    a *= 2
-    assert a == StaticArray([2, 4, "a", 2, 4, "a"])
+        assert array([1, 3, 5]) * 3 == array([1, 3, 5, 1, 3, 5, 1, 3, 5])
+        a = array([2, 4, "a"])
+        a *= 2
+        assert a == array([2, 4, "a", 2, 4, "a"])
+
+    assert StaticArray([1, 2, 3], 6) + StaticArray([4, 5, 6], 5) == StaticArray([1, 2, 3, 4, 5, 6], 7)
 
 
 def test_in():
-    a = StaticArray([1, 0.4, [1, 3], [], "1"])
-    assert 1 in a
-    assert 2 not in a
-    assert [1, 3] in a
-    assert [] in a
-    assert [1] not in a
-    assert "1" in a
+    for array in ds:
+        a = array([1, 0.4, [1, 3], [], "1"])
+        assert 1 in a
+        assert 2 not in a
+        assert [1, 3] in a
+        assert [] in a
+        assert [1] not in a
+        assert "1" in a
 
 
 def test_compare():
-    assert StaticArray([1, 2, 3]) == StaticArray([1, 2, 3], 4)
-    assert StaticArray([]) != StaticArray([1])
-    assert StaticArray([1, 2]) < StaticArray([3])
-    assert StaticArray([1, 2]) <= StaticArray([3])
-    assert StaticArray([]) >= StaticArray([])
-    assert StaticArray([4, 4, 4]) >= StaticArray([4, 3])
+    for array in ds:
+        assert array([1, 2, 3]) == array([1, 2, 3])
+        assert array([]) != array([1])
+        assert array([1, 2]) < array([3])
+        assert array([1, 2]) <= array([3])
+        assert array([]) >= array([])
+        assert array([4, 4, 4]) >= array([4, 3])
+        assert array([]) != []
 
 
 def test_del():
-    a = StaticArray([1, 2, 3], 4)
-    del a[1]
-    assert a == StaticArray([1, 3])
+    for array in ds:
+        a = array([1, 2, 3])
+        del a[1]
+        assert a == array([1, 3])
 
-    def _test():
-        del a.copy
+        def _test():
+            del a.copy
 
-    def _test2():
-        del a.max_length
+        def _test2():
+            del a.max_length
 
-    is_error(AttributeError, _test)
-    is_error(AttributeError, _test2)
+        is_error(AttributeError, _test)
+        is_error(AttributeError, _test2)
 
 
 def test_get():
-    a = StaticArray(["a", "c", "b", "d"])
-    assert a[2] == "b"
-    assert a[0:4] == ["a", "c", "b", "d"]
-    assert a[0::2] == ["a", "b"]
-    assert a[5::] == []
-    assert a.max_length == 4
+    for array in ds:
+        a = array(["a", "c", "b", "d"])
+        assert a[2] == "b"
+        assert a[0:4] == ["a", "c", "b", "d"]
+        assert a[0::2] == ["a", "b"]
+        assert a[5::] == []
+        assert a.max_length == 4
 
-    def _test():
-        return a[10]
+        def _test():
+            return a[10]
 
-    is_error(IndexError, _test)
+        is_error(IndexError, _test)
 
 
 def test_iter():
-    arr = [4, 3, 2, 5, 5, 4, 3]
-    a = StaticArray(arr)
-    for idx, item in enumerate(a):
-        assert item == arr[idx]
+    for array in ds:
+        arr = [4, 3, 2, 5, 5, 4, 3]
+        a = array(arr)
+        for idx, item in enumerate(a):
+            assert item == arr[idx]
 
 
 def test_len():
+    for array in ds:
+        assert len(array([])) == 0
+        assert len(array([1])) == 1
+
     assert len(StaticArray([], 10)) == 0
-    assert len(StaticArray([1])) == 1
 
 
 def test_repr_str():
-    a = StaticArray([1, "Test", set(), [], {1: "10"}, 3.2])
-    assert repr(a) == "StaticArray([1, 'Test', set(), [], {1: '10'}, 3.2], 6)"
-    assert str(a) == "[1, 'Test', set(), [], {1: '10'}, 3.2]"
+    arr = [1, "Test", set(), [], {1: "10"}, 3.2]
+    for array in ds:
+        a = array(arr)
+        assert str(a) == "[1, 'Test', set(), [], {1: '10'}, 3.2]"
+
+    assert repr(StaticArray(arr)) == "StaticArray([1, 'Test', set(), [], {1: '10'}, 3.2], 6)"
+    assert repr(DynamicArray(arr)) == "DynamicArray([1, 'Test', set(), [], {1: '10'}, 3.2], 6)"
 
 
 def test_reversed():
     a = StaticArray([1, "Test", set(), [], {1: "10"}, 3.2])
     assert repr(StaticArray(reversed(a))) == "StaticArray([3.2, {1: '10'}, [], set(), 'Test', 1], 6)"
 
+    a = DynamicArray([1, "Test", set(), [], {1: "10"}, 3.2])
+    assert repr(DynamicArray(reversed(a))) == "DynamicArray([3.2, {1: '10'}, [], set(), 'Test', 1], 6)"
+
 
 def test_set():
-    a = StaticArray([1, 2, 0.3])
-    a.some_random_stuff = 1
+    for array in ds:
+        a = array([1, 2, 0.3])
+        a.some_random_stuff = 1
 
     def _test():
-        a.max_length = 10
+        StaticArray([1, 2, 3]).max_length = 10
 
     is_error(ConstantError, _test)
 
@@ -114,6 +140,20 @@ def test_append():
     b.append(9)
     assert b == StaticArray([1, 2, 3, 9])
 
+    c = DynamicArray([])
+    c.append(10)
+    assert c == DynamicArray([10])
+    assert c.max_length == 1
+    assert c != StaticArray([10])
+    c.append(20)
+    c.append(30)
+    assert c == DynamicArray([10, 20, 30])
+    assert c.max_length == 4
+    c.append(40)
+    c.append(50)
+    assert c == DynamicArray([10, 20, 30, 40, 50])
+    assert c.max_length == 8
+
 
 def test_clear():
     a = StaticArray([1, 2, 3, 4, 5])
@@ -121,26 +161,40 @@ def test_clear():
     assert a == StaticArray([])
     assert a.max_length == 5
 
+    b = DynamicArray([1, 2, 3, 4, 5])
+    b.clear()
+    assert b == DynamicArray([])
+    assert b.max_length == 0
+
 
 def test_copy():
     a = StaticArray([1], 10)
     b = a.copy()
-
     assert a == b
     assert a is not b
     b.append(2)
     assert b == StaticArray([1, 2])
     assert a == StaticArray([1])
 
+    c = DynamicArray([1])
+    d = c.copy()
+
+    assert c == d
+    assert c is not d
+    d.append(2)
+    assert d == DynamicArray([1, 2])
+    assert c == DynamicArray([1])
+
 
 def test_count():
-    a = StaticArray([])
-    assert a.count(0) == 0
+    for array in ds:
+        a = array([])
+        assert a.count(0) == 0
 
-    b = StaticArray([1, 1, 2, 1, 1, 2, 3])
-    assert b.count(1) == 4
-    assert b.count(2) == 2
-    assert b.count(3) == 1
+        b = array([1, 1, 2, 1, 1, 2, 3])
+        assert b.count(1) == 4
+        assert b.count(2) == 2
+        assert b.count(3) == 1
 
 
 def test_extend():
@@ -152,15 +206,27 @@ def test_extend():
     assert b == StaticArray([1, 2, 3, 4, 5, 4, 3, 2])
     is_error(ExceedMaxLengthError, b.extend, [0, 0, "exceed"])
 
+    c = DynamicArray([])
+    c.extend([1])
+    assert c == DynamicArray([1])
+    assert c.max_length == 1
+
+    d = DynamicArray([1, 2])
+    d.extend(reversed(d))
+    d.extend([3, 4, 5, 6, 7, 8, 9])
+    assert d == DynamicArray([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    assert d.max_length == 16
+
 
 def test_index():
-    a = StaticArray()
-    is_error(ValueError, a.index, "")
+    for array in ds:
+        a = array()
+        is_error(ValueError, a.index, "")
 
-    b = StaticArray([1, 2, 2, 2, 2, 3])
-    assert b.index(1) == 0
-    assert b.index(2) == 1
-    assert b.index(3) == 5
+        b = array([1, 2, 2, 2, 2, 3])
+        assert b.index(1) == 0
+        assert b.index(2) == 1
+        assert b.index(3) == 5
 
 
 def test_insert():
@@ -172,48 +238,69 @@ def test_insert():
     a.insert(1, 15)
     assert a == StaticArray([20, 15, 10])
 
+    b = DynamicArray()
+    b.insert(0, 10)
+    assert b == DynamicArray([10])
+    assert b.max_length == 1
+    b.insert(-1, 20)
+    assert b == DynamicArray([20, 10])
+    assert b.max_length == 2
+    b.insert(1, 15)
+    assert b == DynamicArray([20, 15, 10])
+    assert b.max_length == 4
+
 
 def test_pop():
-    a = StaticArray([20, 18, 15, 10])
-    a.pop(0)
-    assert a == StaticArray([18, 15, 10])
-    a.pop(-1)
-    assert a == StaticArray([18, 15])
-    a.pop(1)
-    assert a == StaticArray([18])
-    a.pop()
-    assert a == StaticArray([])
-
-    def _test():
+    for array in ds:
+        a = array([20, 18, 15, 10])
+        a.pop(0)
+        assert a == array([18, 15, 10])
+        a.pop(-1)
+        assert a == array([18, 15])
+        a.pop(1)
+        assert a == array([18])
         a.pop()
+        assert a == array([])
 
-    is_error(IndexError, _test)
+        def _test():
+            a.pop()
+
+        is_error(IndexError, _test)
 
 
 def test_remove():
-    a = StaticArray([])
+    for array in ds:
+        a = array([])
 
-    def _test():
-        a.remove(10)
+        def _test():
+            a.remove(10)
 
-    is_error(ValueError, _test)
-    b = StaticArray([2, 2, 2, 2])
-    b.remove(2)
-    assert b == StaticArray([2, 2, 2])
+        is_error(ValueError, _test)
+        b = array([2, 2, 2, 2])
+        b.remove(2)
+        assert b == array([2, 2, 2])
+
+    c = DynamicArray([1, 2, 3, 3, 3, 3, 3, 3])
+    for _ in range(6):
+        c.remove(3)
+    assert c == DynamicArray([1, 2])
+    assert c.max_length == 2
 
 
 def test_reverse():
-    a = StaticArray([])
-    a.reverse()
-    assert a == StaticArray()
+    for array in ds:
+        a = array([])
+        a.reverse()
+        assert a == array()
 
-    b = StaticArray([2, 3, 1])
-    b.reverse()
-    assert b == StaticArray([1, 3, 2])
+        b = array([2, 3, 1])
+        b.reverse()
+        assert b == array([1, 3, 2])
 
 
 def test_sort():
-    a = StaticArray([4, 5, 2, 3, 2])
-    a.sort()
-    assert a == StaticArray([2, 2, 3, 4, 5])
-    assert a.max_length == 5
+    for array in ds:
+        a = array([4, 5, 2, 3, 2])
+        a.sort()
+        assert a == array([2, 2, 3, 4, 5])
+        assert a.max_length == 5
