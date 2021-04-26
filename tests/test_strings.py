dd13ec14a085a5361dfa53da92e608397008e4f1
jaredliw <jaredliw@gmail.com>
2021-04-26 20:31:37 +0800

    Remove file

--- tests/test_strings.py
+++
@@ -1,6 +0,0 @@
-from pydsa.algorithms.string import *
-
-
-def test_is_pangram():
-    assert is_pangram("The quick brown fox jumps over the lazy dog 1234!")
-    assert not is_pangram("hello world!")
